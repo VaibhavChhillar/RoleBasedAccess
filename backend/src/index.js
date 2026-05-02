792502a7617c0b8fcb6bcb5c8a550ed5b52470d6
Vaibhav Chhillar <131567808+VaibhavChhillar@users.noreply.github.com>
2026-05-02 15:42:57 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -38,4 +38,6 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
